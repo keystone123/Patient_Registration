6dad43d1a04d47bafd2b4a0ea2248307c5f79e5e
keystone123 <arunkumar@keystone-healthcare.com>
2017-11-16 10:08:55 +0530

    commit

--- main.php
+++ main.php
@@ -1,85 +1,93 @@
 <?php
-   class patient {
-      public $fname = "";
-      public $mname  = "";
-      public $lname = "";
-	  public $dob = "";
-	  public $age = "";
-      public $gender = "";
-	  public $mobile = "";
-      public $email = "";
-	  public $bloodgroup = "";
-	  public $consultant = "";
-	  public $refering_doctor = "";
-	  public $patient_code = "";
-      public $organization_code = "";
-	  public $staff_number = "";
-	  public $next_of_kin = "";
-	  public $relationship = "";
-	  public $old_regno = "";
-
-	  
-	  public $marital_status = "";
-	  public $house_number = "";
-	  public $street_name = "";
-	  public $area = "";
-	  public $zip_code = "";
-	  public $city = "";
-	  public $state = "";
-	  public $country = "";
-	  public $reg_date = "";
-	  public $validity_date = "";
-	  
-	  
-	  public $emergency = "";
-	  public $relation = "";
-	  public $phoneno = "";
-	  public $religion = "";
-	  public $education = "";
-	  public $occupation = "";
-	  public $monthly_income = "";
-	  
+   
+   if(isset($_POST['fname'])=="")
+   {
+	 echo '<script type="text/javascript">';  
+     echo 'window.location.href = "index.html";';
+     echo '</script>';
    }
-	
-   $p = new patient();
-   $p->fname = $_POST['fname'];
-   $p->mname  = $_POST['mname'];
-   $p->lname = $_POST['lname'];
-   $p->dob = $_POST['dob'];
-   $p->age = $_POST['age'];
-   $p->gender = $_POST['gender'];
-   $p->mobile = $_POST['mobile'];
-   $p->email = $_POST['email'];
-   $p->bloodgroup = $_POST['blood'];
-   $p->consultant = $_POST['con'];
-   $p->refering_doctor = $_POST['doc'];
-   $p->patient_code = $_POST['patient'];
-   $p->organization_code = $_POST['org'];
-   $p->staff_number = $_POST['staff'];
-   $p->next_of_kin = $_POST['kin'];
-   $p->relationship = $_POST['rel'];
-   $p->old_regno = $_POST['old'];
+   {
+   $fname = $_POST['fname'];
+   $mname  = $_POST['mname'];
+   $lname = $_POST['lname'];
+   $dob = $_POST['dob'];
+   $age = $_POST['age'];
+   $gender = $_POST['gender'];
+   $mobile = $_POST['mobile'];
+   $email = $_POST['email'];
+   $bloodgroup = $_POST['blood'];
+   $consultant = $_POST['con'];
+   $refering_doctor = $_POST['doc'];
+   $patient_code = $_POST['patient'];
+   $organization_code = $_POST['org'];
+   $staff_number = $_POST['staff'];
+   $next_of_kin = $_POST['kin'];
+   $relationship = $_POST['rel'];
+   $old_regno = $_POST['old'];
    
    
-   $p->marital_status = $_POST['marital'];
-   $p->house_number = $_POST['house'];
-   $p->street_name = $_POST['street'];
-   $p->area = $_POST['area'];
-   $p->zip_code = $_POST['zip'];
-   $p->city = $_POST['city'];
-   $p->state = $_POST['state'];
-   $p->country = $_POST['country'];
-   $p->reg_date = $_POST['reg'];
-   $p->validity_date = $_POST['val'];
+   $marital_status = $_POST['marital'];
+   $house_number = $_POST['house'];
+   $street_name = $_POST['street'];
+   $area = $_POST['area'];
+   $zip_code = $_POST['zip'];
+   $city = $_POST['city'];
+   $state = $_POST['state'];
+   $country = $_POST['country'];
+   $reg_date = $_POST['reg'];
+   $validity_date = $_POST['val'];
    
    
-   $p->emergency = $_POST['emergency'];
-   $p->relation = $_POST['relation'];
-   $p->phoneno = $_POST['phoneno'];
-   $p->religion = $_POST['religion'];
-   $p->education = $_POST['education'];
-   $p->occupation = $_POST['occupation'];
-   $p->monthly_income = $_POST['income'];
-    
-  echo json_encode($p);
-?>
+   $emergency = $_POST['emergency'];
+   $relation = $_POST['relation'];
+   $phoneno = $_POST['phoneno'];
+   $religion = $_POST['religion'];
+   $education = $_POST['education'];
+   $occupation = $_POST['occupation'];
+   $monthly_income = $_POST['income'];
+   
+   
+   $arr = array(
+   "fname" => $fname,
+   "mname" => $mname,
+   "lname" => $lname,
+   "dob"   => $dob,
+   "age"   => $age,
+   "gender"   => $gender,
+   "mobile"   => $mobile,
+   "email"   => $email,
+   "bloodgroup"   => $bloodgroup,
+   "consultant"   => $consultant,
+   "refering_doctor"   => $refering_doctor,
+   "patient_code"   => $patient_code,
+   "organization_code"   => $organization_code,
+   "staff_number"   => $staff_number,
+   "next_of_kin"   => $next_of_kin,
+   "relationship"   => $relationship,
+   "old_regno"   => $old_regno,
+   "marital_status"   => $marital_status,
+   "house_number"   => $house_number,
+   "street_name"   => $street_name,
+   "area"   => $area,
+   "zip_code"   => $zip_code,
+   "city"   => $city,
+   "state"   => $state,
+   "country"   => $country,
+   "reg_date"   => $reg_date,
+   "validity_date"   => $validity_date,
+   "emergency"   => $emergency,
+   "relation"   => $relation,
+   "phoneno"   => $phoneno,
+   "religion"   => $religion,
+   "education"   => $education,
+   "occupation"   => $occupation,
+   "monthly_income"   => $monthly_income
+   );
+   $data[] = $arr;
+   
+ echo json_encode($data);
+     echo '<script type="text/javascript">';  
+     echo 'window.location.href = "";';
+     echo '</script>';
+   }
+  ?>
